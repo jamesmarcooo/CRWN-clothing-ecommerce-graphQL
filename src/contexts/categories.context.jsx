963ae2f52e17e2821754c832295357eca6e351fe
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-10-25 02:32:11 +0800

    Add graphql query in categories context

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,21 +1,41 @@
 import { createContext, useState, useEffect } from 'react';
-
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.js';
+import { gql, useQuery } from '@apollo/client';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
-    getCategoriesMap();
-  }, []);
+    if (data) {
+      const { collections } = data;
+      const collectionMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      });
+
+      setCategoriesMap(collectionMap);
+    }
+  }, [data]);
 
   const value = { categoriesMap };
   return (
